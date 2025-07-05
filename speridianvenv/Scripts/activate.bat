aa085997920b92a3d8782303f67393926fbc7f5c
aharonmathews <aharonmathews04@gmail.com>
2025-07-05 10:51:30 +0530

    Now start

--- speridianvenv/Scripts/activate.bat
+++ speridianvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\aharo\Projects\Personal\speridian\speridianproject\speridianvenv"
+set "VIRTUAL_ENV=C:\Users\aharo\Projects\Personal\speridian\SperidianProject\speridianvenv"
 
 if not defined PROMPT set PROMPT=$P$G
 
